0777c8ad9986acfc1a4369b42df098c8067ccd40
Lewis Liu <develop@lewisl.net>
2016-06-14 15:03:22 -0700

    Refactored out special case for aged brie

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,21 +15,17 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
-const QUALITY_CHANGE_RATE = 1;
-const SELL_IN_CHANGE_RATE = 1;
+const QUALITY_CHANGE_RATE = -1;
+const SELL_IN_CHANGE_RATE = -1;
 
 const getNewQuality = (quality, sellIn, qualityChangeRate = QUALITY_CHANGE_RATE) => {
   const newQuality = sellIn < 0 ? 
-    quality - qualityChangeRate * 2 : 
-    quality - qualityChangeRate;
-  return newQuality < 0 ? 0 : newQuality; 
-};
+    quality + qualityChangeRate * 2 : 
+    quality + qualityChangeRate;
 
-// assuming Aged Brie only increases quality at regular rate
-//  regardless of age, even when age is negative
-const getNewAgedBrieQuality = quality => {
-  const newQuality = quality + QUALITY_CHANGE_RATE;
-  return newQuality > 50 ? 50 : newQuality;
+  return newQuality < 0 ? 0 :
+    newQuality > 50 ? 50 :
+    newQuality;
 };
 
 // assuming concert occurs at sell_in = 0
@@ -38,12 +34,12 @@ const getNewBackstagePassQuality = (quality, sellIn) => {
   const newQuality = sellIn < 0 ? 0 :
     sellIn <= 5 ? quality + 3 :
     sellIn <= 10 ? quality + 2 :
-    quality + QUALITY_CHANGE_RATE;
+    quality + QUALITY_CHANGE_RATE * -1;
 
   return newQuality > 50 ? 50 : newQuality;
 };
 
-const getNewSellIn = sellIn => sellIn - SELL_IN_CHANGE_RATE;
+const getNewSellIn = sellIn => sellIn + SELL_IN_CHANGE_RATE;
 
 const updateQuality = item => {
   // assuming items of the same type like backstage passes and conjured items
@@ -67,7 +63,7 @@ const updateQuality = item => {
     case 'Aged Brie':
       return Object.assign({}, item, {
         sell_in: getNewSellIn(item.sell_in),
-        quality: getNewAgedBrieQuality(item.quality)
+        quality: getNewQuality(item.quality, item.sell_in, QUALITY_CHANGE_RATE * -1)
       });
     case 'Sulfuras, Hand of Ragnaros':
       return item;
