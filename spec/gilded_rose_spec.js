2b7f4ddbe6e328cfe9d4c0dca2a68b5b229d212c
Lewis Liu <develop@lewisl.net>
2016-06-14 14:14:58 -0700

    Added aged brie rules

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,28 +1,42 @@
-describe("Gilded Rose", function() {
+describe('Gilded Rose', function() {
 
-  it("should do something", function() {
+  it('should do something', function() {
     console.log(update_quality());
   });
 
-  it("should lower both values", () => {
-    const testItem = new Item("test", 10, 20);
+  it('should lower both values', () => {
+    const testItem = new Item('test', 10, 20);
     const updatedTestItem = updateQuality(testItem);
 
     expect(updatedTestItem.sell_in).toBe(9);
     expect(updatedTestItem.quality).toBe(19);
   });
 
-  it("should never set negative quality", () => {
+  it('should never set negative quality', () => {
     const quality = 0;
     const sellIn = 1;
 
     expect(getNewQuality(quality, sellIn)).toBe(0);
   });
 
-  it("should degrade quality twice as fast when sell_in is less than 0", () => {
+  it('should degrade quality twice as fast when sell_in is less than 0', () => {
     const quality = 3;
     const sellIn = -1;
 
     expect(getNewQuality(quality, sellIn)).toBe(1);
+  });
+
+  it('should increase "Aged Brie" quality', () => {
+    const testItem = new Item('Aged Brie', 2, 0);
+    const updatedTestItem = updateQuality(testItem);
+
+    expect(updatedTestItem.quality).toBe(1);
+  });
+
+  it('should never increase quality past 50', () => {
+    const testItem = new Item('Aged Brie', 2, 50);
+    const updatedTestItem = updateQuality(testItem);
+
+    expect(updatedTestItem.quality).toBe(50);
   });
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,18 +15,38 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
+const qualityChangeRate = 1;
+const sellInChangeRate = 1;
+
 const getNewQuality = (quality, sellIn) => {
-  const newQuality = sellIn < 0 ? quality - 2 : quality - 1;
+  const newQuality = sellIn < 0 ? 
+    quality - qualityChangeRate * 2 : 
+    quality - qualityChangeRate;
   return newQuality < 0 ? 0 : newQuality; 
 };
 
-const getNewSellIn = sellIn => sellIn - 1;
+// assuming Aged Brie only increases quality at regular rate
+// regardless of age, even when age is negative
+const getNewAgedBrieQuality = quality => {
+  const newQuality = quality + qualityChangeRate;
+  return newQuality > 50 ? 50 : newQuality;
+};
+
+const getNewSellIn = sellIn => sellIn - sellInChangeRate;
 
 const updateQuality = item => {
-  return Object.assign({}, item, {
-    sell_in: getNewSellIn(item.sell_in),
-    quality: getNewQuality(item.quality, item.sell_in)
-  });
+  switch (item.name) {
+    case 'Aged Brie':
+      return Object.assign({}, item, {
+        quality: getNewAgedBrieQuality(item.quality),
+        sell_in: getNewSellIn(item.sell_in)
+      });
+    default:
+      return Object.assign({}, item, {
+        quality: getNewQuality(item.quality, item.sell_in),
+        sell_in: getNewSellIn(item.sell_in)
+      });
+  }
 };
 
 function update_quality() {
