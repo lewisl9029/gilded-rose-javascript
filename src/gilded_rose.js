60e28cfcd0b44ef5672f6743435004ee05e66fd5
Lewis Liu <develop@lewisl.net>
2016-06-14 15:20:38 -0700

    Adding more optional change rate arguments

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -31,16 +31,16 @@ const getNewQuality = (quality, sellIn, qualityChangeRate = QUALITY_CHANGE_RATE)
 
 // assuming concert occurs at sell_in = 0
 // assuming quality increases at regular rate when sell_in > 10
-const getNewBackstagePassQuality = (quality, sellIn) => {
+const getNewBackstagePassQuality = (quality, sellIn, qualityChangeRate = QUALITY_CHANGE_RATE) => {
   const newQuality = sellIn < 0 ? 0 :
     sellIn <= 5 ? quality + 3 :
     sellIn <= 10 ? quality + 2 :
-    quality + QUALITY_CHANGE_RATE * -1;
+    quality + qualityChangeRate * -1;
 
   return newQuality > 50 ? 50 : newQuality;
 };
 
-const getNewSellIn = sellIn => sellIn + SELL_IN_CHANGE_RATE;
+const getNewSellIn = (sellIn, sellInChangeRate = SELL_IN_CHANGE_RATE) => sellIn + sellInChangeRate;
 
 const updateQuality = item => {
   // assuming items of the same type like backstage passes and conjured items
@@ -77,5 +77,8 @@ const updateQuality = item => {
 };
 
 function update_quality() {
+  // modified to return a new set of items instead of mutating in-place
+  // using plain JS objects & arrays for simplicity
+  // would use a immutable collections library like ImmutableJS or Mori in production
   return items.map(updateQuality);
 }
